19dfb37256fee28aaddc8678b5cd2c59d360eed0
udondon3 <udondon02@gmail.com>
2024-09-06 13:14:35 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 // ハンバーガーメニューボタンとナビゲーションを取得
 const btnGnavi = document.querySelector('.btn-gnavi');
 const nav = document.querySelector('nav.gnavi');
@@ -14,3 +15,12 @@ closeBtn.addEventListener('click', () => {
   btnGnavi.classList.remove('open');
   nav.classList.remove('open');
 });
+
+// 位置スクロール
+function scrollToSection(event, sectionId) {
+  event.preventDefault(); 
+  const section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' }); 
+  }
+}
